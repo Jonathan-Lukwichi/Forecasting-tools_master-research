59c151665ac7f5cd82248f2f1a0ae4fb94fe027d
Jonathan Lukwichi <jonathanlukwichi29@@email.com>
2026-03-17 10:12:29 +0200

    Update: frontend\src\components\layout\AppShell.tsx [2026-03-17 10:12]

--- frontend/src/components/layout/AppShell.tsx
+++ frontend/src/components/layout/AppShell.tsx
@@ -7,7 +7,9 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <Sidebar />
-      <div className="ml-56 flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col lg:ml-56">
+        {/* Top spacing for mobile hamburger */}
+        <div className="h-14 lg:hidden" />
         <main className="flex-1">{children}</main>
         <PageNavigation />
       </div>
